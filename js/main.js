c94af4f69c53b661790902c5c41b72ec936dd6bb
Fariha Chowdhury <56296730+fariha009@users.noreply.github.com>
2021-06-24 11:10:41 +1000

    Happy to publish
    portfolio

---
+++ js/main.js
@@ -0,0 +1,30 @@
+// navbar starts
+const slide = ()=>{
+    const hamburger = document.querySelector(".hamburger");
+    const items = document.querySelector(".nav-items");
+
+    hamburger.addEventListener('click', ()=>{
+        items.classList.toggle('nav-active');
+        hamburger.classList.toggle('change');
+    });
+
+
+
+}
+slide();
+
+AOS.init({
+    easing:'ease',
+    duration:1500
+});
+
+
+$(window).load(function() {
+    $("body").removeClass("preload");
+  });
+
+  
+function topFunction(){
+    document.body.scrollTop = 50;
+    document.documentElement.scrollTop = 50;
+}
